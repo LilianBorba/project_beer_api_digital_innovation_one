fbeab15fc62e69bb9b6d96b038c354251f4efedf
Lilian Borba <60194456+LilianBorba@users.noreply.github.com>
2025-10-12 18:58:06 -0300

    Update BeerDTO.java

--- src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
+++ src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
@@ -3,7 +3,44 @@ package com.dio.beerstock.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;apackage com.dio.beerstock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String name;
+
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String brand;
+
+    @NotNull
+    @Max(500)
+    private Integer max;
+
+    @NotNull
+    @Max(100)
+    private Integer quantity;
+
+    @NotNull
+    private String type; // Ex: Lager, Ale, IPA
+}
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
