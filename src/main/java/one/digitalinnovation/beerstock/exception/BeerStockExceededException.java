9d3bbf9c7e0078387d446449dabf21bb9e05da2a
Lilian Borba <60194456+LilianBorba@users.noreply.github.com>
2025-10-12 19:01:13 -0300

    Update BeerStockExceededException.java

--- src/main/java/one/digitalinnovation/beerstock/exception/BeerStockExceededException.java
+++ src/main/java/one/digitalinnovation/beerstock/exception/BeerStockExceededException.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.beerstock.exception;
+package com.dio.beerstock.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class BeerStockExceededException extends Exception {
 
     public BeerStockExceededException(Long id, int quantityToIncrement) {
-        super(String.format("Beers with %s ID to increment informed exceeds the max stock capacity: %s", id, quantityToIncrement));
+        super(String.format("Beer with ID %d stock exceeded. Requested: %d.", id, quantityToIncrement));
     }
 }
