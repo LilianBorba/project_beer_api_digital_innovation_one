8954c4f0f6e2a881862aa6a58ee946713973f047
Lilian Borba <60194456+LilianBorba@users.noreply.github.com>
2025-10-12 19:00:34 -0300

    Update BeerNotFoundException.java

--- src/main/java/one/digitalinnovation/beerstock/exception/BeerNotFoundException.java
+++ src/main/java/one/digitalinnovation/beerstock/exception/BeerNotFoundException.java
@@ -1,16 +1,13 @@
-package one.digitalinnovation.beerstock.exception;
+package com.dio.beerstock.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class BeerNotFoundException extends Exception {
-
-    public BeerNotFoundException(String beerName) {
-        super(String.format("Beer with name %s not found in the system.", beerName));
-    }
-
-    public BeerNotFoundException(Long id) {
-        super(String.format("Beer with id %s not found in the system.", id));
+    
+    // Construtor para busca por ID ou por Nome
+    public BeerNotFoundException(Object identifier) {
+        super(String.format("Beer with identifier %s not found in the system.", identifier.toString()));
     }
 }
