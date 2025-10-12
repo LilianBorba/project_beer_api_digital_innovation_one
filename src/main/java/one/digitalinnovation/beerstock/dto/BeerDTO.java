64824957ba7f5d075586ae09a223e0b5dc501771
Lilian Borba <60194456+LilianBorba@users.noreply.github.com>
2025-10-12 18:44:55 -0300

    Update BeerDTO.java

--- src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
+++ src/main/java/one/digitalinnovation/beerstock/dto/BeerDTO.java
@@ -1,13 +1,10 @@
-package one.digitalinnovation.beerstock.dto;
+package com.dio.beerstock.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import one.digitalinnovation.beerstock.enums.BeerType;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -36,7 +33,6 @@ public class BeerDTO {
     @Max(100)
     private Integer quantity;
 
-    @Enumerated(EnumType.STRING)
     @NotNull
-    private BeerType type;
+    private String type; // Ex: Lager, Ale, IPA
 }
